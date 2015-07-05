b11bb16c4fd66f67763d395c02d8255bcb052bdf
erenon <erenon2@gmail.com>
2015-07-05 16:09:59 +0200

    devector: add license headers

--- include/boost/container/devector.hpp
+++ include/boost/container/devector.hpp
@@ -1,3 +1,13 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Benedek Thaler 2015-2015. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/container for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
 #ifndef BOOST_CONTAINER_CONTAINER_DEVECTOR_HPP
 #define BOOST_CONTAINER_CONTAINER_DEVECTOR_HPP
 
