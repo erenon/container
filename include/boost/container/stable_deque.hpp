5a70b98dbc25177dc9cacad448d7f1e3714cdb71
erenon <erenon2@gmail.com>
2015-07-27 21:17:52 +0200

    stable_deque: initial commit

---
+++ include/boost/container/stable_deque.hpp
@@ -0,0 +1,672 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Benedek Thaler 2015-2015. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/container for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_CONTAINER_CONTAINER_FLEX_DEQUE_HPP
+#define BOOST_CONTAINER_CONTAINER_FLEX_DEQUE_HPP
+
+#include <memory>
+
+#include <boost/container/devector.hpp>
+
+namespace boost {
+namespace container {
+
+template <std::size_t SegmentSize>
+struct stable_deque_policy
+{
+  static const unsigned segment_size = SegmentSize;
+};
+
+template <
+  typename T,
+  typename Allocator = std::allocator<T>,
+  typename FlexDequePolicy = stable_deque_policy<512>
+>
+class stable_deque : Allocator
+{
+  typedef std::unique_ptr<T[]> segment;
+
+  struct allocator_traits : public std::allocator_traits<Allocator>
+  {
+    // before C++14, std::allocator does not specify propagate_on_container_move_assignment,
+    // std::allocator_traits sets it to false. Not something we would like to use.
+    static constexpr bool propagate_on_move_assignment =
+       std::allocator_traits<Allocator>::propagate_on_container_move_assignment::value
+    || std::is_same<Allocator, std::allocator<T>>::value;
+
+    // poor emulation of a C++17 feature
+    static constexpr bool is_always_equal = std::is_same<Allocator, std::allocator<T>>::value;
+  };
+
+  typedef typename allocator_traits::template rebind_alloc<segment> map_allocator;
+
+  typedef devector<segment, devector_small_buffer_policy<0>, devector_growth_policy, map_allocator> map_t;
+  typedef typename map_t::iterator map_iterator;
+
+  static constexpr unsigned segment_size = FlexDequePolicy::segment_size;
+
+  static_assert(segment_size > 0, "Segment size must be greater than 0");
+
+public:
+
+  typedef T value_type;
+  typedef Allocator allocator_type;
+
+  typedef value_type& reference;
+  typedef const value_type& const_reference;
+  typedef typename allocator_traits::pointer pointer;
+  typedef typename allocator_traits::const_pointer const_pointer;
+
+  typedef BOOST_CONTAINER_IMPDEF(std::size_t)    size_type;
+  typedef BOOST_CONTAINER_IMPDEF(std::ptrdiff_t) difference_type;
+
+  /**
+   * @tparam Container stable_deque or const stable_deque
+   * @tparam U T or const T
+   */
+  template <bool IsConst = false>
+  class deque_iterator :
+    public std::iterator
+    <
+      std::random_access_iterator_tag,
+      typename std::conditional<IsConst, const T, T>::type
+    >
+  {
+    typedef typename std::conditional<IsConst, const stable_deque, stable_deque>::type* container_pointer;
+
+    // TODO iterator missing members
+  public:
+    deque_iterator() {}
+
+    deque_iterator(container_pointer container, pointer segment, unsigned elem_index)
+      :_container(container),
+       _segment(segment),
+       _index(elem_index)
+    {}
+
+    reference operator*()
+    {
+      return _segment[_index];
+    }
+
+    deque_iterator& operator++()
+    {
+      ++_index;
+      if (_index == segment_size)
+      {
+        _segment = next_segment();
+        _index = 0;
+      }
+      return *this;
+    }
+
+    deque_iterator& operator--()
+    {
+      if (_index != 0)
+      {
+        --_index;
+      }
+      else
+      {
+        _segment = prev_segment();
+        _index = segment_size - 1;
+      }
+      return *this;
+    }
+
+    pointer data()
+    {
+      return _segment + _index;
+    }
+
+    const_pointer data() const
+    {
+      return _segment + _index;
+    }
+
+    size_type data_size() const
+    {
+      // *this must not be singular
+      return (_container->_map.back().get() != _segment)
+        ? segment_size - _index
+        :_container->_back_index - _index;
+    }
+
+    friend bool operator==(const deque_iterator& a, const deque_iterator& b)
+    {
+      // No need to compare _container, comparing iterators
+      // of different sequences is undefined.
+      return
+             a._segment == b._segment
+        &&   a._index == b._index;
+    }
+
+    friend bool operator!=(const deque_iterator& a, const deque_iterator& b)
+    {
+      return !(a == b);
+    }
+
+    friend difference_type operator-(const deque_iterator& a, const deque_iterator& b)
+    {
+      size_type seg_a = a.segment_index();
+      size_type seg_b = b.segment_index();
+
+      return difference_type(segment_size)
+        * (seg_a - seg_b - 1) + a._index + (segment_size - b._index);
+    }
+
+    #ifdef BOOST_CONTAINER_TEST
+
+    friend std::ostream& operator<<(std::ostream& out, const deque_iterator& i)
+    {
+      out << "[ " << i._container << ", " << i._segment << ", " << i._index << " ]";
+      return out;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const std::reverse_iterator<deque_iterator>& ri)
+    {
+      auto&& i = ri.base();
+      out << "[ " << i._container << ", " << i._segment << ", " << i._index << " ]";
+      return out;
+    }
+
+    #endif
+
+  private:
+    pointer next_segment()
+    {
+      return next_segment_in_range(
+        _container->_map.begin(),
+        _container->_map.end()
+      );
+    }
+
+    pointer prev_segment()
+    {
+      return next_segment_in_range(
+        _container->_map.rbegin(),
+        _container->_map.rend()
+      );
+    }
+
+    template <typename Iterator>
+    pointer next_segment_in_range(Iterator first, Iterator last)
+    {
+      pointer result = nullptr;
+
+      for (; first != last; ++first)
+      {
+        if (_segment == first->get())
+        {
+          ++first;
+          break;
+        }
+      }
+
+      if (first != last)
+      {
+        result = first->get();
+      }
+
+      return result;
+    }
+
+    size_type segment_index() const
+    {
+      size_type i = 0;
+      for (; i < _container->_map.size(); ++i)
+      {
+        if (_container->_map[i].get() == _segment)
+        {
+          break;
+        }
+      }
+
+      return i;
+    }
+
+    container_pointer _container;
+    pointer _segment = nullptr;
+    unsigned _index = 0;
+  };
+
+  typedef BOOST_CONTAINER_IMPDEF(deque_iterator<false>) iterator;
+  typedef BOOST_CONTAINER_IMPDEF(deque_iterator<true>) const_iterator;
+  typedef std::reverse_iterator<iterator> reverse_iterator;
+  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+  // construct/copy/destroy:
+  stable_deque() : stable_deque(Allocator()) {}
+
+  explicit stable_deque(const Allocator& allocator) noexcept
+    :Allocator(allocator),
+     _front_index(0),
+     _back_index(segment_size)
+  {}
+
+  explicit stable_deque(size_type n, const Allocator& = Allocator());
+
+  stable_deque(size_type n, const T& value, const Allocator& = Allocator());
+
+  template <class InputIterator>
+  stable_deque(InputIterator first, InputIterator last, const Allocator& = Allocator());
+
+  stable_deque(const stable_deque& x);
+  stable_deque(const stable_deque&, const Allocator&);
+
+  stable_deque(stable_deque&&);
+  stable_deque(stable_deque&&, const Allocator&);
+
+  stable_deque(std::initializer_list<T>, const Allocator& = Allocator());
+
+  ~stable_deque()
+  {
+    destroy_elements(begin(), end());
+  }
+
+  stable_deque& operator=(const stable_deque& x);
+  stable_deque& operator=(stable_deque&& x) noexcept(allocator_traits::is_always_equal);
+  stable_deque& operator=(std::initializer_list<T>);
+  template <class InputIterator>
+  void assign(InputIterator first, InputIterator last);
+  void assign(size_type n, const T& t);
+  void assign(std::initializer_list<T>);
+
+  allocator_type get_allocator() const noexcept
+  {
+    return static_cast<const Allocator&>(*this);
+  }
+
+  // iterators:
+  iterator begin() noexcept
+  {
+    return begin_impl();
+  }
+
+  const_iterator begin() const noexcept
+  {
+    return begin_impl();
+  }
+
+  iterator end() noexcept
+  {
+    return end_impl();
+  }
+
+  const_iterator end() const noexcept
+  {
+    return end_impl();
+  }
+
+  reverse_iterator rbegin() noexcept
+  {
+    return reverse_iterator(end());
+  }
+
+  const_reverse_iterator rbegin() const noexcept
+  {
+    return const_reverse_iterator(end());
+  }
+
+  reverse_iterator rend() noexcept
+  {
+    return reverse_iterator(begin());
+  }
+
+  const_reverse_iterator rend() const noexcept
+  {
+    return const_reverse_iterator(begin());
+  }
+
+  const_iterator cbegin() const noexcept
+  {
+    return begin_impl();
+  }
+
+  const_iterator cend() const noexcept
+  {
+    return end_impl();
+  }
+
+  const_reverse_iterator crbegin() const noexcept
+  {
+    return const_reverse_iterator(end());
+  }
+
+  const_reverse_iterator crend() const noexcept
+  {
+    return const_reverse_iterator(begin());
+  }
+
+  // capacity:
+  bool empty() const noexcept
+  {
+    return _map.empty();
+  }
+
+  size_type size() const noexcept
+  {
+    size_type map_size = _map.size();
+    if (map_size == 0)
+    {
+      return 0;
+    }
+    else if (map_size == 1)
+    {
+      return _back_index - _front_index;
+    }
+    else // map_size >= 2
+    {
+      return
+        (segment_size - _front_index)
+      + _back_index
+      + (map_size - 2) * segment_size;
+    }
+  }
+
+  size_type max_size() const noexcept
+  {
+    return allocator_traits::max_size(get_allocator_ref());
+  }
+
+  void      resize(size_type sz);
+  void      resize(size_type sz, const T& c);
+  void      shrink_to_fit();
+
+  // element access:
+  reference       operator[](size_type n);
+  const_reference operator[](size_type n) const;
+  reference       at(size_type n);
+  const_reference at(size_type n) const;
+
+  reference front() noexcept
+  {
+    return *(_map.front().get() + _front_index);
+  }
+
+  const_reference front() const
+  {
+    return *(_map.front().get() + _front_index);
+  }
+
+  reference back()
+  {
+    return *(_map.back().get() + _back_index);
+  }
+
+  const_reference back() const
+  {
+    return *(_map.back().get() + _back_index);
+  }
+
+  // modifiers:
+  template <class... Args>
+  void emplace_front(Args&&... args)
+  {
+    if (front_free_capacity())
+    {
+      alloc_construct(
+        _map.front().get() + (_front_index - 1),
+        std::forward<Args>(args)...
+      );
+      --_front_index;
+    }
+    else
+    {
+      emplace_front_slow_path(std::forward<Args>(args)...);
+    }
+
+    BOOST_ASSERT(invariants_ok());
+  }
+
+  template <class... Args>
+  void emplace_back(Args&&... args)
+  {
+    if (back_free_capacity())
+    {
+      alloc_construct(
+        _map.back().get() + _back_index,
+        std::forward<Args>(args)...
+      );
+      ++_back_index;
+    }
+    else
+    {
+      emplace_back_slow_path(std::forward<Args>(args)...);
+    }
+
+    BOOST_ASSERT(invariants_ok());
+  }
+
+  template <class... Args>
+  iterator emplace(const_iterator position, Args&&... args);
+
+  void push_front(const T& x);
+  void push_front(T&& x);
+  void push_back(const T& x);
+  void push_back(T&& x);
+
+  iterator insert(const_iterator position, const T& x);
+  iterator insert(const_iterator position, T&& x);
+  iterator insert(const_iterator position, size_type n, const T& x);
+  template <class InputIterator>
+  iterator insert (const_iterator position, InputIterator first, InputIterator last);
+  iterator insert(const_iterator position, std::initializer_list<T>);
+
+  template <class InputIterator>
+  iterator stable_insert (const_iterator position_hint, InputIterator first, InputIterator last);
+
+  void pop_front()
+  {
+    BOOST_ASSERT(!empty());
+
+    allocator_traits::destroy(get_allocator_ref(), _map.front().get() + _front_index);
+    ++_front_index;
+
+    if (_front_index == segment_size)
+    {
+      _map.pop_front();
+      _front_index = 0;
+    }
+
+    BOOST_ASSERT(invariants_ok());
+  }
+
+  void pop_back()
+  {
+    BOOST_ASSERT(!empty());
+
+    --_back_index;
+    allocator_traits::destroy(get_allocator_ref(), _map.back().get() + _back_index);
+
+    if (_back_index == 0)
+    {
+      _map.pop_back();
+      _back_index = segment_size;
+    }
+
+    BOOST_ASSERT(invariants_ok());
+  }
+
+  iterator erase(const_iterator position);
+  iterator erase(const_iterator first, const_iterator last);
+  void     swap(stable_deque&) noexcept(allocator_traits::is_always_equal);
+
+  void clear() noexcept
+  {
+    destroy_elements(begin(), end());
+    _map.clear();
+    _front_index = 0;
+    _back_index = segment_size;
+  }
+
+
+private:
+
+  void destroy_elements(iterator first, iterator last)
+  {
+    for (; first != last; ++first)
+    {
+      allocator_traits::destroy(get_allocator_ref(), &*first);
+    }
+  }
+
+  allocator_type& get_allocator_ref()
+  {
+    return static_cast<allocator_type&>(*this);
+  }
+
+  const allocator_type& get_allocator_ref() const
+  {
+    return static_cast<const allocator_type&>(*this);
+  }
+
+  iterator begin_impl()
+  {
+    if (!empty())
+    {
+      return iterator(this, _map.front().get(), _front_index);
+    }
+
+    return iterator{};
+  }
+
+  const_iterator begin_impl() const
+  {
+    if (!empty())
+    {
+      return const_iterator(this, _map.front().get(), _front_index);
+    }
+
+    return const_iterator{};
+  }
+
+  iterator end_impl()
+  {
+    return iterator{
+      this,
+      _back_index == segment_size ? nullptr : _map.back().get(),
+      _back_index % segment_size
+    };
+  }
+
+  const_iterator end_impl() const
+  {
+    return const_iterator{
+      this,
+      _back_index == segment_size ? nullptr : _map.back().get(),
+      _back_index % segment_size
+    };
+  }
+
+  template <typename... Args>
+  void alloc_construct(pointer dst, Args&&... args)
+  {
+    std::allocator_traits<Allocator>::construct(
+      get_allocator_ref(),
+      dst,
+      std::forward<Args>(args)...
+    );
+  }
+
+  size_type front_free_capacity() const noexcept
+  {
+    return _front_index;
+  }
+
+  size_type back_free_capacity() const noexcept
+  {
+    return segment_size - _back_index;
+  }
+
+  template <typename... Args>
+  void emplace_front_slow_path(Args&&... args)
+  {
+    BOOST_ASSERT(_front_index == 0);
+
+    _map.reserve_front(new_map_capacity());
+    segment new_segment(new T[segment_size]);
+
+    size_type new_front_index = segment_size - 1;
+
+    alloc_construct(
+      new_segment.get() + new_front_index,
+      std::forward<Args>(args)...
+    );
+
+    _map.push_front(std::move(new_segment));
+    _front_index = new_front_index;
+  }
+
+  template <typename... Args>
+  void emplace_back_slow_path(Args&&... args)
+  {
+    BOOST_ASSERT(_back_index == segment_size);
+
+    _map.reserve_back(new_map_capacity());
+    segment new_segment(new T[segment_size]);
+
+    alloc_construct(
+      new_segment.get(),
+      std::forward<Args>(args)...
+    );
+
+    _map.push_back(std::move(new_segment));
+    _back_index = 1;
+  }
+
+  size_type new_map_capacity() const
+  {
+    return (_map.size())
+      ? _map.size() + _map.size() / 2 + 1
+      : 4;
+  }
+
+  bool invariants_ok()
+  {
+    return (! _map.empty() || (_front_index == 0 && _back_index == segment_size))
+      &&   (_map.size() > 1 || _front_index <= _back_index)
+      &&    _front_index < segment_size
+      &&    _back_index > 0;
+  }
+
+  map_t _map;
+  unsigned _front_index;
+  unsigned _back_index;
+};
+
+template <class T, class AX, class PX, class AY, class PY>
+bool operator==(const stable_deque<T, AX, PX>& x, const stable_deque<T, AY, PY>& y);
+
+template <class T, class AX, class PX, class AY, class PY>
+bool operator< (const stable_deque<T, AX, PX>& x, const stable_deque<T, AY, PY>& y);
+
+template <class T, class AX, class PX, class AY, class PY>
+bool operator!=(const stable_deque<T, AX, PX>& x, const stable_deque<T, AY, PY>& y);
+
+template <class T, class AX, class PX, class AY, class PY>
+bool operator> (const stable_deque<T, AX, PX>& x, const stable_deque<T, AY, PY>& y);
+
+template <class T, class AX, class PX, class AY, class PY>
+bool operator>=(const stable_deque<T, AX, PX>& x, const stable_deque<T, AY, PY>& y);
+
+template <class T, class AX, class PX, class AY, class PY>
+bool operator<=(const stable_deque<T, AX, PX>& x, const stable_deque<T, AY, PY>& y);
+
+// specialized algorithms:
+template <class T, class Allocator, class P>
+void swap(stable_deque<T, Allocator>& x, stable_deque<T, Allocator>& y) noexcept(noexcept(x.swap(y)))
+{
+  x.swap(y);
+}
+
+}} // namespace boost::container
+
+#endif // BOOST_CONTAINER_CONTAINER_FLEX_DEQUE_HPP
---
+++ test/stable_deque_test.cpp
@@ -0,0 +1,389 @@
+////////////////////////////////////////////////////////////////////////////////
+////
+//// (C) Copyright Benedek Thaler 2015-2015. Distributed under the Boost
+//// Software License, Version 1.0. (See accompanying file
+//// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+////
+//// See http://www.boost.org/libs/container for documentation.
+////
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+#define BOOST_CONTAINER_TEST
+#include <boost/container/stable_deque.hpp>
+#undef BOOST_CONTAINER_TEST
+
+using namespace boost::container;
+
+#define BOOST_TEST_MODULE stable_deque
+#include <boost/test/included/unit_test.hpp>
+
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/filter_view.hpp>
+#include <boost/mpl/is_sequence.hpp>
+#include <boost/mpl/placeholders.hpp>
+
+#include <boost/algorithm/cxx14/equal.hpp>
+
+template <typename T>
+using make_stable_deque = stable_deque<T, std::allocator<T>, stable_deque_policy<8>>;
+
+typedef boost::mpl::list<
+  make_stable_deque<unsigned>
+  // TODO add other test_elem types
+> all_deques;
+
+// TODO mpl::filter_view does not compile if predicate has template template argument. why?
+//template <template<typename> class Predicate, typename Container>
+//struct if_value_type
+//  : public Predicate<typename Container::value_type>
+//{};
+//
+//typedef boost::mpl::filter_view<all_deques, if_value_type<std::is_default_constructible, boost::mpl::_1>>::type
+//  t_is_default_constructible;
+
+template <typename Container>
+struct is_value_type_default_constructible
+  : public std::is_default_constructible<typename Container::value_type>
+{};
+
+typedef boost::mpl::filter_view<all_deques, is_value_type_default_constructible<boost::mpl::_1>>::type
+  t_is_default_constructible;
+
+template <typename Container>
+struct is_value_type_copy_constructible
+  : public std::is_copy_constructible<typename Container::value_type>
+{};
+
+typedef boost::mpl::filter_view<all_deques, is_value_type_copy_constructible<boost::mpl::_1>>::type
+  t_is_copy_constructible;
+
+template <typename Container>
+struct is_value_type_trivial
+  : public std::is_trivial<typename Container::value_type>
+{};
+
+typedef boost::mpl::filter_view<all_deques, is_value_type_trivial<boost::mpl::_1>>::type
+  t_is_trivial;
+
+template <typename Container>
+Container getRange(int fbeg, int fend, int bbeg, int bend)
+{
+  Container c;
+
+  for (int i = fend; i > fbeg ;)
+  {
+    c.emplace_front(--i);
+  }
+
+  for (int i = bbeg; i < bend; ++i)
+  {
+    c.emplace_back(i);
+  }
+
+  return c;
+}
+
+template <typename Container>
+Container getRange(int count)
+{
+  Container c;
+
+  for (int i = 1; i <= count; ++i)
+  {
+    c.emplace_back(i);
+  }
+
+  return c;
+}
+
+template <typename Container>
+Container getRange()
+{
+  return getRange<Container>(1, 13, 13, 25);
+}
+
+template <typename T, typename A, typename P, typename C2>
+bool operator==(const stable_deque<T, A, P>& a, const C2& b)
+{
+  return std::lexicographical_compare(
+    a.begin(), a.end(),
+    b.begin(), b.end()
+  );
+}
+
+template <typename Range>
+void printRange(std::ostream& out, const Range& range)
+{
+  out << '[';
+  bool first = true;
+  for (auto&& elem : range)
+  {
+    if (first) { first = false; }
+    else { out << ','; }
+
+    out << elem;
+  }
+  out << ']';
+}
+
+template <typename T, typename A, typename P>
+std::ostream& operator<<(std::ostream& out, const stable_deque<T, A, P>& deque)
+{
+  printRange(out, deque);
+  return out;
+}
+
+// END HELPERS
+
+// TODO iterator operations
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(segment_iterator, Deque, t_is_trivial)
+{
+  typedef typename Deque::value_type T;
+
+  auto expected = getRange<devector<T>>();
+  auto a = getRange<Deque>();
+
+  T* p_expected = expected.data();
+
+  auto a_first = a.begin();
+  auto a_last = a.end();
+
+  while (a_first != a_last)
+  {
+    auto size = a_first.data_size();
+    bool ok = std::memcmp(a_first.data(), p_expected, size) == 0;
+    BOOST_TEST(ok);
+
+    // TODO change this if segment subclass completed
+    for (auto i = 0; i < size; ++i)
+    {
+      ++a_first;
+    }
+
+    p_expected += size;
+  }
+
+  BOOST_TEST(p_expected == expected.data() + expected.size());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(consturctor_default, Deque, all_deques)
+{
+  {
+    Deque a;
+
+    BOOST_TEST(a.size() == 0);
+    BOOST_TEST(a.empty());
+  }
+
+  {
+    typename Deque::allocator_type allocator;
+    Deque b(allocator);
+
+    BOOST_TEST(b.size() == 0);
+    BOOST_TEST(b.empty());
+  }
+}
+
+// TODO constructor_n_value
+// TODO constructor_n_copy
+// TODO constructor_input_range
+// TODO constructor_forward_range
+// TODO copy_constructor
+// TODO move_constructor
+// TODO constructor_il
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(destructor, Deque, all_deques)
+{
+  { Deque a; }
+  {
+    Deque b = getRange<Deque>();
+  }
+}
+
+// TODO op_copy_assign
+// TODO op_move_assign
+// TODO op_assign_il
+// TODO assign_input_range
+// TODO assign_forward_range
+// TODO assign_n_copy
+// TODO assign_in
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(get_allocator, Deque, all_deques)
+{
+  Deque a;
+  (void)a.get_allocator();
+}
+
+template <typename Deque, typename MutableDeque>
+void test_begin_end_impl()
+{
+  {
+    Deque a;
+    BOOST_TEST(a.begin() == a.end());
+  }
+
+  {
+    Deque b = getRange<MutableDeque>(1, 13, 13, 25);
+    auto expected = getRange<std::vector<typename Deque::value_type>>(24);
+
+    BOOST_TEST(boost::algorithm::equal(
+      b.begin(), b.end(),
+      expected.begin(), expected.end()
+    ));
+  }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(begin_end, Deque, all_deques)
+{
+  test_begin_end_impl<      Deque, Deque>();
+  test_begin_end_impl<const Deque, Deque>();
+}
+
+template <typename Deque, typename MutableDeque>
+void test_rbegin_rend_impl()
+{
+  {
+    Deque a;
+    BOOST_TEST(a.rbegin() == a.rend());
+  }
+
+  {
+    Deque b = getRange<MutableDeque>(1, 13, 13, 25);
+    auto expected = getRange<std::vector<typename Deque::value_type>>(24);
+
+    BOOST_TEST(boost::algorithm::equal(
+      b.rbegin(), b.rend(),
+      expected.rbegin(), expected.rend()
+    ));
+  }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(rbegin_rend, Deque, all_deques)
+{
+  test_rbegin_rend_impl<      Deque, Deque>();
+  test_rbegin_rend_impl<const Deque, Deque>();
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(cbegin_cend, Deque, all_deques)
+{
+  {
+    Deque a;
+    BOOST_TEST(a.cbegin() == a.cend());
+  }
+
+  {
+    Deque b = getRange<Deque>(1, 13, 13, 25);
+    auto expected = getRange<std::vector<typename Deque::value_type>>(24);
+
+    BOOST_TEST(boost::algorithm::equal(
+      b.cbegin(), b.cend(),
+      expected.cbegin(), expected.cend()
+    ));
+  }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(crbegin_crend, Deque, all_deques)
+{
+  {
+    Deque a;
+    BOOST_TEST(a.crbegin() == a.crend());
+  }
+
+  {
+    Deque b = getRange<Deque>(1, 13, 13, 25);
+    auto expected = getRange<std::vector<typename Deque::value_type>>(24);
+
+    BOOST_TEST(boost::algorithm::equal(
+      b.crbegin(), b.crend(),
+      expected.crbegin(), expected.crend()
+    ));
+  }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(empty, Deque, all_deques)
+{
+  Deque a;
+  BOOST_TEST(a.empty());
+
+  a.emplace_front(1);
+
+  BOOST_TEST(! a.empty());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(size, Deque, all_deques)
+{
+  Deque a;
+  BOOST_TEST(a.size() == 0);
+
+  a.emplace_front(1);
+  a.emplace_front(2);
+  a.emplace_front(3);
+
+  BOOST_TEST(a.size() == 3);
+
+  a.pop_front();
+  a.pop_front();
+
+  BOOST_TEST(a.size() == 1);
+
+  a.emplace_back(2);
+  a.emplace_back(3);
+  a.emplace_back(4);
+  a.emplace_back(5);
+  a.emplace_back(6);
+
+  BOOST_TEST(a.size() == 6);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(max_size, Deque, all_deques)
+{
+  Deque a;
+  (void)a.max_size();
+}
+
+// TODO resize_value
+// TODO resize_copy
+// TODO shrink_to_fit
+
+// TODO op_at -- const, non const
+// TODO at -- const, non const
+
+// TODO front -- const, non const
+// TODO back -- const, non const
+
+// TODO emplace_front
+// TODO push_front_copy
+// TODO push_front_rvalue
+
+// TODO emplace_back
+// TODO push_back_copy
+// TODO push_back_rvalue
+
+// TODO emplace
+
+// TODO insert_copy
+// TODO insert_rvalue
+// TODO insert_n_copy
+// TODO insert_input_range
+// TODO insert_forward_range
+// TODO insert_il
+// TODO stable_insert
+
+// TODO pop_front
+// TODO pop_back
+
+// TODO erase
+// TODO erase_range
+// TODO swap
+// TODO clear
+
+// TODO op_eq
+// TODO op_lt
+// TODO op_ne
+// TODO op_gt
+// TODO op_ge
+// TODO op_le
+// TODO free_swap
