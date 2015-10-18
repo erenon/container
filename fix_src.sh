327320ce35cfbe46627ec3f33e064ccc18d8d47c
erenon <erenon2@gmail.com>
2015-10-18 22:24:17 +0100

    Update to d360aefd412269c31b6789785494278c3f3ace0f

--- fix_src.sh
+++ fix_src.sh
@@ -2,4 +2,5 @@
 
 for file in $(find . -name "*.html"); do
   sed -i -e 's/src="\(..\/\)\+/src="http:\/\/www.boost.org\/doc\/libs\/1_57_0\//g' $file
+  sed -i -e 's/href="\(..\/\)\+/href="http:\/\/www.boost.org\/doc\/libs\/1_57_0\//g' $file
 done
